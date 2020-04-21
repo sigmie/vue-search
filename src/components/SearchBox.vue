6daac9f5e6641eb751f7370da39eae9d72117711
Nico <nico_orfi@yahoo.com>
2020-04-21 09:48:09 +0200

    UPD: Initial commit

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="sigmie-search-box">
+    <input type="text" name="search" id="search-box" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#sigmie-search-box {
+  background-color:yellow;
+}
+</style>
