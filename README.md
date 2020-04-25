ba626fb34cf78e4092f9dfb9a7a0e63e88ef17bc
Nico <nico_orfi@yahoo.com>
2020-04-25 22:38:20 +0200

    UPD: styles

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-# vue-search
+# Sigmie vue-search-box
 
-## Project setup
-```
-yarn install
-```
+Vue search box made for Sigmie Elasticsearch integrations.
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="sigmie-search-box">
     <input type="text" name="search" id="search-box" />
+    <div id="results">
+      <div id="list">list</div>
+      <div id="preview">preview</div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +13,24 @@ export default {};
 </script>
 
 <style>
-#sigmie-search-box {
-  background-color:yellow;
+input[type="text"] {
+  width: 200px;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 1px solid #edf2f7;
+}
+
+#list {
+  display: inline-block;
+}
+
+#preview {
+  display: inline-block;
+}
+
+#results {
+  width: 200px;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
 }
 </style>
